5301f6de0ce53a25f140c2c1e66e516615795a3e
Manu_Dev <65360943+Emanuele-maker@users.noreply.github.com>
2021-09-07 14:14:00 +0200

    update server.js

--- server.js
+++ server.js
@@ -57,6 +57,6 @@ io.on("connection", socket => {
     })
 })
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
